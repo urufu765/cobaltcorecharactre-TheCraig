4c967fafa91d59a57bc01753cb618d241b6ba09e
rft50 <62362611+rft50@users.noreply.github.com>
2024-09-26 17:01:28 -0500

    Foundation

---
+++ ModEntry.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace DemoMod;
+
+internal class ModEntry : SimpleMod
+{
+    internal static ModEntry Instance { get; private set; } = null!;
+
+    public ModEntry(IPluginPackage<IModManifest> package, IModHelper helper, ILogger logger) : base(package, helper, logger)
+    {
+        Instance = this;
+    }
+}
+
